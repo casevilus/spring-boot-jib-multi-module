f42a0ca8075d4dee06d029d6ff710eac38542216
Can Sevilmis <csevilmis@avaya.com>
2020-08-18 14:01:54 +0100

    helmignore added

--- microservice/src/main/java/com/example/DemoApplication.java
+++ microservice/src/main/java/com/example/DemoApplication.java
@@ -3,21 +3,28 @@ package com.example; /**
  * @date 8/17/20
  */
 
+import com.example.configuration.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class DemoApplication {
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-    public static void main(String[] args){
-        SpringApplication.run(DemoApplication.class, args);
+    @Autowired
+    ApplicationContext applicationContext;
 
-        
+    public static void main(String[] args){
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+        AppConfig appConfig = ctx.getBean(AppConfig.class);
+        appConfig.blobServiceClient();
+
+
     }
 }
